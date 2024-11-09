0e09ea7ff4a6cf4601d7f1f4a5b4bb20c748fbd3
Loris QUESADO <loris.quesado@oclock.school>
2024-11-09 15:57:43 +0100

    (streaming): handle server side loading state - cls issue

--- app/(main)/streaming/page.tsx
+++ app/(main)/streaming/page.tsx
@@ -1,25 +1,30 @@
 import LoadTimeTracker from "@/components/ui/LoadtimeTracker";
+import { Suspense } from "react";
 import ProductWrapper from "./_components/wrapper/ProductWrapper";
 import ReviewWrapper from "./_components/wrapper/ReviewWrapper";
 
-// The first issue was that multiple await that doesn't depend on each other are creating a waterfall issue
-// The page made 5s to render
-// Since we aren't fetching any data into our page, we could remove the async key word and make its content static at build time
+// By handling the loading state and making a UI feedback to the user, we strongly reduce the FCP / LCP
 const Page = () => (
   <main className="container grow px-4 mx-auto my-8">
     <section>
       <h2 className="text-2xl mb-4 font-bold">ProductList</h2>
       <div className="grid grid-cols-12 gap-10">
-        {/* By creating wrappers and isolate our request, we can parallelize our api calls */}
-        <ProductWrapper />
+        {/* We are simply using the Suspense component from React to handle the loading state */}
+        {/* The fallback is the content that'll render during the loading state */}
+        <Suspense fallback={<>Loading Products...</>}>
+          <ProductWrapper />
+        </Suspense>
       </div>
     </section>
 
     <section>
       <h2 className="text-2xl my-4 font-bold">Reviews ⭐</h2>
       <ul className="grid grid-cols-12 gap-4">
-        {/* So this request doesn't need to wait after ProductWrapper to finish rendering to start */}
-        <ReviewWrapper />
+        {/* !! But, as you can see, there's a lot of CLS (Cumulative Layout Shift) !! */}
+        {/* Reviews are pushed down when the Product promise is resolved. As you may guess, it is really bad for the UX (User Experience) */}
+        <Suspense fallback={<>Loading Reviews...</>}>
+          <ReviewWrapper />
+        </Suspense>
       </ul>
     </section>
     <LoadTimeTracker />
